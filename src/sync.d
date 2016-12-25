bbbfeb68f3a1b2e841636c6dd933b7a89c16bff1
skilion <andrea.9940@gmail.com>
2016-12-25 20:17:02 +0100

    fix condition in applyDifferences loop

--- src/sync.d
+++ src/sync.d
@@ -109,7 +109,7 @@ final class SyncEngine
 				}
 				statusToken = changes["@delta.token"].str;
 				std.file.write(cfg.statusTokenFilePath, statusToken);
-			} while ((changes.type != JSON_TYPE.OBJECT) && (("@odata.nextLink" in changes) !is null));
+			} while (!((changes.type == JSON_TYPE.OBJECT) && (("@odata.nextLink" in changes) is null)));
 		} catch (ErrnoException e) {
 			throw new SyncException(e.msg, e);
 		} catch (FileException e) {
