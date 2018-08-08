1e5739692a0fa67a378484f884f5f66af61160ab
abraunegg <alex.braunegg@gmail.com>
2018-08-08 12:36:54 +1000

    updates
    updates

--- tests/makefiles.sh
+++ tests/makefiles.sh
@@ -53,7 +53,7 @@ touch "${BADFILES}/LPT9"
 
 # Test files from cases
 # File contains invalid whitespace characters
-tar xf ./bad-file-name.tar.xz -C ${BADFILES}/
+tar xf ~/tests/bad-file-name.tar.xz -C ${BADFILES}/
 
 # HelloCOM2.rar should be allowed
 dd if=/dev/urandom of=${TESTFILES}/HelloCOM2.rar count=5 bs=1572864
