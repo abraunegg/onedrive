31d15886bd9c0cf7e9689f65df1700400eaa3b8c
abraunegg <alex.braunegg@gmail.com>
2018-08-26 07:06:04 +1000

    update debugging
    update debugging

--- src/itemdb.d
+++ src/itemdb.d
@@ -179,17 +179,12 @@ final class ItemDatabase
 		path = "root/" ~ path.chompPrefix(".");
 		auto s = db.prepare("SELECT * FROM item WHERE name = ?1 AND driveId IS ?2 AND parentId IS ?3");
 		foreach (name; pathSplitter(path)) {
-		
-			// Issue #121 Debugging
-			log.log("Path: ", name);
-			log.log("driveId: ", currItem.driveId);
-			log.log("itemId: ", currItem.id);
-		
 			s.bind(1, name);
 			s.bind(2, currItem.driveId);
 			s.bind(3, currItem.id);
 			auto r = s.exec();
 			if (r.empty) {
+				// Issue #121 Debugging
 				log.log("ERROR - This item was not found in the database: ", name ~ ", " ~ currItem.driveId ~ ", " ~ currItem.id);
 				return false;
 			}
