db78ed37541a72ad5492adadfb339764497f936d
abraunegg <alex.braunegg@gmail.com>
2018-10-24 10:00:50 +1100

    Update src/onedrive.d
    * refactor PR104 for latest code base

--- src/onedrive.d
+++ src/onedrive.d
@@ -13,10 +13,14 @@ private immutable {
 	string authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
 	string redirectUrl = "https://login.microsoftonline.com/common/oauth2/nativeclient";
 	string tokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-	string driveUrl = "https://graph.microsoft.com/v1.0/me/drive";
-	string itemByIdUrl = "https://graph.microsoft.com/v1.0/me/drive/items/";
-	string itemByPathUrl = "https://graph.microsoft.com/v1.0/me/drive/root:/";
 	string driveByIdUrl = "https://graph.microsoft.com/v1.0/drives/";
+}
+
+private {
+    string driveUrl = "https://graph.microsoft.com/v1.0/me/drive";
+    string itemByIdUrl = "https://graph.microsoft.com/v1.0/me/drive/items/";
+    string itemByPathUrl = "https://graph.microsoft.com/v1.0/me/drive/root:/";
+    string driveId = "";
 }
 
 class OneDriveException: Exception
@@ -66,6 +70,15 @@ final class OneDriveApi
 
 	bool init()
 	{
+		try {
+			driveId = cfg.getValue("drive_id");
+			if (driveId.length) {
+				driveUrl = driveByIdUrl ~ driveId;
+                itemByIdUrl = driveUrl ~ "/items";
+                itemByPathUrl = driveUrl ~ "/root:/";
+			}
+		} catch (Exception e) {}
+	
 		try {
 			refreshToken = readText(cfg.refreshTokenFilePath);
 		} catch (FileException e) {
