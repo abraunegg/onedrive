ffc20ff3872c291046cea03fe0f84f7bcfff515f
Norbert Preining <norbert@preining.info>
2018-12-13 12:38:05 +0900

    add pkg-config to guest dependencies

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -16,7 +16,7 @@ DEBIAN_MIRROR="http://ftp.us.debian.org/debian"
 HOST_DEPENDENCIES="qemu-user-static binfmt-support debootstrap sbuild wget"
 
 # Debian package dependencies for the chrooted environment
-GUEST_DEPENDENCIES="build-essential libcurl4-openssl-dev libsqlite3-dev libgnutls-openssl27 git"
+GUEST_DEPENDENCIES="build-essential libcurl4-openssl-dev libsqlite3-dev libgnutls-openssl27 git pkg-config"
 
 function setup_arm32_chroot {
 	# Update apt repository details
