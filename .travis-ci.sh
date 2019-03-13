d8ae9d4aa02e4390a2713493bb2bd92db70b7374
abraunegg <alex.braunegg@gmail.com>
2019-03-14 07:07:48 +1100

    Update .travis-ci.sh
    * Update ARMHF and AARM64 to use difference versions of LDC due to due to https://github.com/ldc-developers/ldc/issues/3027

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -19,7 +19,9 @@ HOST_DEPENDENCIES="qemu-user-static binfmt-support debootstrap sbuild wget"
 GUEST_DEPENDENCIES="build-essential libcurl4-openssl-dev libsqlite3-dev libgnutls-openssl27 git libxml2"
 
 # LDC Version
-LDC_VERSION=1.13.0
+# Different versions due to https://github.com/ldc-developers/ldc/issues/3027
+LDC_VERSION_ARMHF=1.13.0
+LDC_VERSION_ARM64=1.14.0
 
 function setup_arm32_chroot {
 	# Update apt repository details
@@ -30,10 +32,10 @@ function setup_arm32_chroot {
 	# Host dependencies
 	sudo apt-get install -qq -y ${HOST_DEPENDENCIES}
 	# Download LDC compiler
-	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-linux-armhf.tar.xz
-	tar -xf ldc2-${LDC_VERSION}-linux-armhf.tar.xz
-	mv ldc2-${LDC_VERSION}-linux-armhf dlang-${ARCH}
-	rm -rf ldc2-${LDC_VERSION}-linux-armhf.tar.xz
+	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION_ARMHF}/ldc2-${LDC_VERSION_ARMHF}-linux-armhf.tar.xz
+	tar -xf ldc2-${LDC_VERSION_ARMHF}-linux-armhf.tar.xz
+	mv ldc2-${LDC_VERSION_ARMHF}-linux-armhf dlang-${ARCH}
+	rm -rf ldc2-${LDC_VERSION_ARMHF}-linux-armhf.tar.xz
 	# Create chrooted environment
 	sudo mkdir ${CHROOT_DIR}
 	sudo debootstrap --foreign --no-check-gpg --variant=buildd --arch=${CHROOT_ARCH} ${VERSION} ${CHROOT_DIR} ${DEBIAN_MIRROR}
@@ -52,10 +54,10 @@ function setup_arm64_chroot {
 	# Host dependencies
 	sudo apt-get install -qq -y ${HOST_DEPENDENCIES}
 	# Download LDC compiler
-	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-linux-aarch64.tar.xz
-	tar -xf ldc2-${LDC_VERSION}-linux-aarch64.tar.xz
-	mv ldc2-${LDC_VERSION}-linux-aarch64 dlang-${ARCH}
-	rm -rf ldc2-${LDC_VERSION}-linux-aarch64.tar.xz
+	wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION_ARM64}/ldc2-${LDC_VERSION_ARM64}-linux-aarch64.tar.xz
+	tar -xf ldc2-${LDC_VERSION_ARM64}-linux-aarch64.tar.xz
+	mv ldc2-${LDC_VERSION_ARM64}-linux-aarch64 dlang-${ARCH}
+	rm -rf ldc2-${LDC_VERSION_ARM64}-linux-aarch64.tar.xz
 	
 	# ARM64 qemu-debootstrap needs to be 1.0.78, Trusty is 1.0.59
 	#sudo echo "deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse" >> /etc/apt/sources.list
