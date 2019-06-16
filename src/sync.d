10a2d9fbe52679de2691cdb607da2465793313bf
abraunegg <alex.braunegg@gmail.com>
2019-06-17 04:25:42 +1000

    Size check should only be used if configured
    * Size check should only be used if configured

--- src/sync.d
+++ src/sync.d
@@ -963,9 +963,11 @@ final class SyncEngine
 			log.vdebug("OneDrive change is a new local item");
 			// Check if file should be skipped based on size limit
 			if (isItemFile(driveItem)) {
-				if (onedrive.getFileDetails(item.driveId, item.id)["size"].integer >= this.newSizeLimit) {
-					log.vlog("Skipping item - excluded by skip_size config: ", item.name, " (", onedrive.getFileDetails(item.driveId, item.id)["size"].integer/2^^20, " MB)");
-					return;
+				if (cfg.getValueLong("skip_size") != 0) {
+					if (onedrive.getFileDetails(item.driveId, item.id)["size"].integer >= this.newSizeLimit) {
+						log.vlog("Skipping item - excluded by skip_size config: ", item.name, " (", onedrive.getFileDetails(item.driveId, item.id)["size"].integer/2^^20, " MB)");
+						return;
+					}
 				}
 			}
 			applyNewItem(item, path);
