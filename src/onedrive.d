690f9463d5071a78831c685667b61caca8d20085
abraunegg <alex.braunegg@gmail.com>
2019-08-09 09:15:13 +1000

    Update onedrive.d
    * Update scopes as per https://github.com/OneDrive/onedrive-api-docs/issues/1119

--- src/onedrive.d
+++ src/onedrive.d
@@ -166,7 +166,7 @@ final class OneDriveApi
 	{
 		import std.stdio, std.regex;
 		char[] response;
-		string url = authUrl ~ "?client_id=" ~ clientId ~ "&scope=files.readwrite%20files.readwrite.all%20offline_access&response_type=code&redirect_uri=" ~ redirectUrl;
+		string url = authUrl ~ "?client_id=" ~ clientId ~ "&scope=Files.ReadWrite%20Files.ReadWrite.all%20Sites.ReadWrite.All%20offline_access&response_type=code&redirect_uri=" ~ redirectUrl;
 		string authFilesString = cfg.getValueString("auth_files");
 		if (authFilesString == "") {
 			log.log("Authorize this app visiting:\n");
