92fd8f19ed7943a8644c7af3f01d6c03b6292c9e
abraunegg <alex.braunegg@gmail.com>
2019-09-23 16:11:49 +1000

    Update entrypoint.sh
    * Change verbosity to be normal by default
    * Add debug option
    * Add resync option

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -20,7 +20,7 @@ if ! oduser="$(getent passwd $ONEDRIVE_UID)"; then
 else
   oduser="${oduser%%:*}"
   usermod -g "${odgroup}" "${oduser}"
-  grep -qv root <( groups "${oduser}" ) || { echo 'ROOT level priviledges prohibited!'; exit 1; }
+  grep -qv root <( groups "${oduser}" ) || { echo 'ROOT level privileges prohibited!'; exit 1; }
 fi
 
 chown "${oduser}:${odgroup}" /onedrive/ /onedrive/conf
@@ -28,12 +28,22 @@ chown "${oduser}:${odgroup}" /onedrive/ /onedrive/conf
 # Default parameters
 ARGS=(--monitor --confdir /onedrive/conf --syncdir /onedrive/data)
 
-#Make Verbose output optional, based on an environment variable
-# default behaviour is verbose (to continue to behave as before)
-if ! [ "${ONEDRIVE_VERBOSE:=1}" == "0" ]; then
+# Make Verbose output optional, based on an environment variable
+if [ "${ONEDRIVE_VERBOSE:=0}" == "1" ]; then
    echo "# We are being verbose"
-   echo "# set ONEDRIVE_VERBOSE environment to 0 (Zero) to be less chatty"
    ARGS=(--verbose ${ARGS[@]})
+fi
+
+# Tell client to perform debug output, based on an environment variable
+if [ "${ONEDRIVE_DEBUG:=0}" == "1" ]; then
+   echo "# We are performing debug output"
+   ARGS=(--verbose --verbose ${ARGS[@]})
+fi
+
+# Tell client to perform a resync based on environment variable
+if [ "${ONEDRIVE_RESYNC:=0}" == "1" ]; then
+   echo "# We are performing a --resync"
+   ARGS=(--resync ${ARGS[@]})
 fi
 
 if [ ${#} -gt 0 ]; then
