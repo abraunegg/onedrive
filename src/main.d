abraunegg/onedrive

commit 80675aa8b4f5abac61b3708443d387a8b41f97d2

Fix unable to download all files (Issue #563) (#568)
* Update logging output for better clarity
* Update logging output from vdebug -> error for errors
* Remove restrictive checks on fileDetails - if missing, continue, not return
* Add debug lines for if / when this is missing

src/sync.d
@@ -328,6 +328,7 @@ final class SyncEngine
 			}
 			
 			// Display accountType, defaultDriveId, defaultRootId & remainingFreeSpace for verbose logging purposes
+			log.vlog("Application version: ", strip(import("version")));
 			log.vlog("Account Type: ", accountType);
 			log.vlog("Default Drive ID: ", defaultDriveId);
 			log.vlog("Default Root ID: ", defaultRootId);
@@ -396,10 +397,13 @@ final class SyncEngine
 		string rootId = defaultRootId;
 		applyDifferences(driveId, rootId);
 
-		// check all remote folders
+		// Check OneDrive Personal Shared Folders
 		// https://github.com/OneDrive/onedrive-api-docs/issues/764
 		Item[] items = itemdb.selectRemoteItems();
-		foreach (item; items) applyDifferences(item.remoteDriveId, item.remoteId);
+		foreach (item; items) {
+			log.vlog("Syncing OneDrive Shared Folder: ", item.name);
+			applyDifferences(item.remoteDriveId, item.remoteId);
+		}
 	}
 
 	// download all new changes from a specified folder on OneDrive
@@ -1109,13 +1113,9 @@ final class SyncEngine
 		try {
 			fileDetails = onedrive.getFileDetails(item.driveId, item.id);
 		} catch (OneDriveException e) {
+			log.error("ERROR: Query of OneDrive for file details failed");
 			if (e.httpStatusCode >= 500) {
 				// OneDrive returned a 'HTTP 5xx Server Side Error' - gracefully handling error - error message already logged
-				downloadFailed = true;
-				return;
-			} else {
-				// Default operation if not a 500 error
-				log.error("ERROR: Query of OneDrive for file details failed");
 				downloadFailed = true;
 				return;
 			}
@@ -1132,7 +1132,7 @@ final class SyncEngine
 			}
 		} else {
 			// Issue #550 handling
-			log.vdebug("ERROR: onedrive.getFileDetails call returned a OneDriveException error");
+			log.error("ERROR: onedrive.getFileDetails call returned an invalid JSON Object");
 			// We want to return, cant download
 			downloadFailed = true;
 			return;
@@ -1141,17 +1141,22 @@ final class SyncEngine
 		if (!dryRun) {
 			ulong fileSize = 0;
 			string OneDriveFileHash;
-			if ( (hasFileSize(fileDetails)) && (hasQuickXorHash(fileDetails)) && (fileDetails.type() == JSONType.object) ) {
-				// fileDetails is a valid JSON object with the elements we need
-				// Set the file size from the returned data
+			
+			// fileDetails should be a valid JSON due to prior check
+			if (hasFileSize(fileDetails)) {
+				// Use the configured filesize as reported by OneDrive
 				fileSize = fileDetails["size"].integer;
+			} else {
+				// filesize missing
+				log.vdebug("WARNING: fileDetails['size'] is missing");
+			}
+			
+			if (hasQuickXorHash(fileDetails)) {
+				// Use the configured quickXorHash as reported by OneDrive
 				OneDriveFileHash = fileDetails["file"]["hashes"]["quickXorHash"].str;
 			} else {
-				// Issue #540 handling
-				log.vdebug("ERROR: onedrive.getFileDetails call returned a OneDriveException error");
-				// We want to return, cant download
-				downloadFailed = true;
-				return;
+				// filesize missing
+				log.vdebug("WARNING: fileDetails['file']['hashes']['quickXorHash'] is missing");
 			}
 			
 			try {

commit 87cae13b7caa22e58b38da74413fb8729a9e45d4

Update sync_list handling
* Update sync_list handling so delta changes are handled first, to capture any file deletes (bug introduced by #568 fix)
* Rework fix for #568 so that if a directory is moved / created in sync_list scope, on the 'true-up' sync, any files that were moved with the folder are now downloaded

src/main.d
@@ -852,8 +852,8 @@ void performSync(SyncEngine sync, string singleDirectory, bool downloadOnly, boo
 					} else {
 						// sync from OneDrive first before uploading files to OneDrive
 						if (logLevel < MONITOR_LOG_SILENT) log.log("Syncing changes from OneDrive ...");
-						// if sync_list is configured, syncListConfigured = true, thus a FULL walk of all OneDrive objects will be performed
-						sync.applyDifferences(syncListConfigured);
+						// For the initial sync, always use the delta link so that we capture all the right delta changes including adds, moves & deletes
+						sync.applyDifferences(false);
 						// Is a full scan of the entire sync_dir required?
 						if (fullScanRequired) {
 							// is this a download only request?						
@@ -862,9 +862,9 @@ void performSync(SyncEngine sync, string singleDirectory, bool downloadOnly, boo
 								// in monitor mode all local changes are captured via inotify
 								// thus scanning every 'monitor_interval' (default 45 seconds) for local changes is excessive and not required
 								sync.scanForDifferences(localPath);
-								// ensure that the current remote state is updated locally
-								// for the 'true-up' set this to false so that we use the delta link for second operation
-								sync.applyDifferences(false);
+								// ensure that the current remote state is updated locally to ensure everything is consistent
+								// for the 'true-up' sync, if sync_list is configured, syncListConfigured = true, thus a FULL walk of all OneDrive objects will be requested and used if required
+								sync.applyDifferences(syncListConfigured);
 							}
 						}
 					}

src/sync.d
@@ -233,6 +233,8 @@ final class SyncEngine
 	private bool syncBusinessFolders = false;
 	// single directory scope flag
 	private bool singleDirectoryScope = false;
+	// sync_list new folder added, trigger delta scan override
+	private bool syncListFullScanTrigger = false;
 
 	this(Config cfg, OneDriveApi onedrive, ItemDatabase itemdb, SelectiveSync selectiveSync)
 	{
@@ -408,6 +410,22 @@ final class SyncEngine
 	{
 		disableUploadValidation = true;
 		log.vdebug("documentLibrary account type - flagging to disable upload validation checks due to Microsoft SharePoint file modification enrichments");
+	}
+	
+	// Issue #568 Handling
+	// If an existing folder is moved into a sync_list valid path (where it previously was out of scope due to sync_list), 
+	// then set this flag to true, so that on the second 'true-up' sync, we force a rescan of the OneDrive path to capture any 'files'
+	void setSyncListFullScanTrigger()
+	{
+		syncListFullScanTrigger = true;
+		log.vdebug("Setting syncListFullScanTrigger = true due to new folder creation request in a location that is in-scope via sync_list");
+	}
+	
+	// unset method
+	void unsetSyncListFullScanTrigger()
+	{
+		syncListFullScanTrigger = false;
+		log.vdebug("Setting syncListFullScanTrigger = false");
 	}
 	
 	// download all new changes from OneDrive
@@ -733,9 +751,10 @@ final class SyncEngine
 			log.vdebug("onedrive.getPathDetailsById call returned an invalid JSON Object");
 		}
 		
+		// Issue #568
 		// If we are using a sync_list file, using deltaLink will actually 'miss' changes (moves & deletes) on OneDrive as using sync_list discards changes
 		// Use the performFullItemScan boolean to control whether we perform a full object scan of use the delta link for the root folder
-		// When using --synchronize the process order is:
+		// When using --synchronize the normal process order is:
 		//   1. Scan OneDrive for changes
 		//   2. Scan local folder for changes
 		//   3. Scan OneDrive for changes
@@ -827,7 +846,7 @@ final class SyncEngine
 			// is changes a valid JSON response
 			if (changes.type() == JSONType.object) {
 				// Are there any changes to process?
-				if ((("value" in changes) != null) && (deltaChanges > 0)) {
+				if ((("value" in changes) != null) && ((deltaChanges > 0) || (syncListFullScanTrigger))) {
 					auto nrChanges = count(changes["value"].array);
 					auto changeCount = 0;
 					
@@ -847,6 +866,11 @@ final class SyncEngine
 						} else {
 							// There are valid changes
 							log.vdebug("Number of items from OneDrive to process: ", nrChanges);
+						}
+						
+						// unset now the full scan trigger if set
+						if (syncListFullScanTrigger) {
+							unsetSyncListFullScanTrigger();
 						}
 					}
 					
@@ -1387,6 +1411,15 @@ final class SyncEngine
 		case ItemType.dir:
 		case ItemType.remote:
 			log.log("Creating directory: ", path);
+			
+			// Issue #568 handling
+			auto syncListExcluded = selectiveSync.isPathExcludedViaSyncList(path);
+			log.vdebug("sync_list excluded: ", syncListExcluded);
+			if (!syncListExcluded) {
+				// path we are creating is not excluded via sync_list
+				setSyncListFullScanTrigger();
+			}
+			
 			if (!dryRun) {
 				mkdirRecurse(path);
 			} else {