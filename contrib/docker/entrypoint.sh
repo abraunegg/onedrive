87f8c18ba74175644ffea526e2180622a49c7d01
abraunegg <alex.braunegg@gmail.com>
2020-04-22 08:22:48 +1000

    Update entrypoint.sh
    * Fix double quote to prevent globbing and word splitting

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -6,7 +6,7 @@ set +H -xeuo pipefail
 : ${ONEDRIVE_GID:=$(stat /onedrive/data -c '%g')}
 
 # Create new group using target GID
-if ! odgroup="$(getent group $ONEDRIVE_GID)"; then
+if ! odgroup="$(getent group "$ONEDRIVE_GID")"; then
   odgroup='onedrive'
   groupadd "${odgroup}" -g "$ONEDRIVE_GID"
 else
@@ -14,7 +14,7 @@ else
 fi
 
 # Create new user using target UID
-if ! oduser="$(getent passwd $ONEDRIVE_UID)"; then
+if ! oduser="$(getent passwd "$ONEDRIVE_UID")"; then
   oduser='onedrive'
   useradd -m "${oduser}" -u "$ONEDRIVE_UID" -g "$ONEDRIVE_GID"
 else
