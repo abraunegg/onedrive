01d0431f7ebec5c46b8b12d6348e89ac0b44933d
abraunegg <alex.braunegg@gmail.com>
2020-08-29 20:57:07 +1000

    Update sync.d
    * Add further debug logging

--- src/sync.d
+++ src/sync.d
@@ -4739,19 +4739,26 @@ final class SyncEngine
 				} else {
 					// Not a 404 response .. is this a 401 response due to some sort of OneDrive Business security policy?
 					if ((e.httpStatusCode == 401) && (accountType != "personal")) {
+						log.vdebug("Received a 401 error response");
 						auto errorArray = splitLines(e.msg);
 						JSONValue errorMessage = parseJSON(replace(e.msg, errorArray[0], ""));
 						if (errorMessage["error"]["message"].str == "Access denied. You do not have permission to perform this action or access this resource.") {
 							// Issue #1041 - Unable to delete OneDrive content when OneDrive Business Retention Policy is enabled
 							try {
+								log.vdebug("Attemtping a reverse delete of objects from OneDrive");
 								foreach_reverse (Item child; children) {
+									log.vdebug("Delete child item from drive: ", child.driveId);
+									log.vdebug("Delete this child item id: ", child.id);
 									onedrive.deleteById(child.driveId, child.id, child.eTag);
 									// delete the child reference in the local database
 									itemdb.deleteById(child.driveId, child.id);
 								}
+								log.vdebug("Delete parent item from drive: ", item.driveId);
+								log.vdebug("Delete this parent item id: ", item.id);
 								onedrive.deleteById(item.driveId, item.id, item.eTag);
 							} catch (OneDriveException e) {
 								// display what the error is
+								log.vdebug("A further error was generated when attempting a reverse delete of objects from OneDrive");
 								displayOneDriveErrorMessage(e.msg);
 								return;
 							}
@@ -4760,19 +4767,26 @@ final class SyncEngine
 				
 					// Not a 404 response .. is this a 403 response due to OneDrive Business Retention Policy being enabled?
 					if ((e.httpStatusCode == 403) && (accountType != "personal")) {
+						log.vdebug("Received a 403 error response");
 						auto errorArray = splitLines(e.msg);
 						JSONValue errorMessage = parseJSON(replace(e.msg, errorArray[0], ""));
 						if (errorMessage["error"]["message"].str == "Request was cancelled by event received. If attempting to delete a non-empty folder, it's possible that it's on hold") {
 							// Issue #338 - Unable to delete OneDrive content when OneDrive Business Retention Policy is enabled
 							try {
+								log.vdebug("Attemtping a reverse delete of objects from OneDrive");
 								foreach_reverse (Item child; children) {
+									log.vdebug("Delete child item from drive: ", child.driveId);
+									log.vdebug("Delete this child item id: ", child.id);
 									onedrive.deleteById(child.driveId, child.id, child.eTag);
 									// delete the child reference in the local database
 									itemdb.deleteById(child.driveId, child.id);
 								}
+								log.vdebug("Delete parent item from drive: ", item.driveId);
+								log.vdebug("Delete this parent item id: ", item.id);
 								onedrive.deleteById(item.driveId, item.id, item.eTag);
 							} catch (OneDriveException e) {
 								// display what the error is
+								log.vdebug("A further error was generated when attempting a reverse delete of objects from OneDrive");
 								displayOneDriveErrorMessage(e.msg);
 								return;
 							}
