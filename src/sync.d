820e5b8557d469f0b1211cbc016e4cf5e9f8385d
wiphi <wiphi@users.noreply.github.com>
2022-03-17 20:20:44 +0100

    Fix logging when download failed

--- src/sync.d
+++ src/sync.d
@@ -2975,6 +2975,9 @@ final class SyncEngine
 		if (!downloadFailed) {
 			writeln("done.");
 			log.fileOnly("Downloading file ", path, " ... done.");
+		} else {
+			writeln("failed!");
+			log.fileOnly("Downloading file ", path, " failed!");
 		}
 	}
 
