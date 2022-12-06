abraunegg/onedrive

commit a348750ec60d8fa5bad08d3df50c04b24c9624aa

Update onedrive.spec.in to fix error building RPM (#2145)
Updating the line
%{_unitdir}/%{name}.service
to 
%{_userunitdir}/%{name}.service

This fixes the issue when building the RPM on RHEL7/8/9 with the following error:
RPM build errors:
    File not found: /root/rpmbuild/BUILDROOT/onedrive-2.4.20-1.el7.x86_64/usr/lib/systemd/system/onedrive.service

This is due to the file actually being created in:
/root/rpmbuild/BUILDROOT/onedrive-2.4.20-1.el7.x86_64/usr/lib/systemd/user/onedrive.service

Changing _unitdir to _userunitdir fixes this error and allows the RPMs to be build correctly.

contrib/spec/onedrive.spec.in
@@ -59,7 +59,7 @@ make
 %{_docdir}/%{name}
 %{_bindir}/%{name}
 %if 0%{?with_systemd}
-%{_unitdir}/%{name}.service
+%{_userunitdir}/%{name}.service
 %{_unitdir}/%{name}@.service
 %else
 %{_bindir}/onedrive_service.sh

commit 59899a79497f06d29e18a786dfb50cf366a443f1

Restore RHEL7 spec file compatibility
* Restore RHEL7 spec file compatibility caused by https://github.com/abraunegg/onedrive/commit/a348750ec60d8fa5bad08d3df50c04b24c9624aa

contrib/spec/onedrive.spec.in
@@ -5,6 +5,12 @@
 %global with_systemd      0
 %endif
 
+%if 0%{?rhel} >= 7
+%global rhel_7      1
+%else
+%global rhel_7      0
+%endif
+
 Name:       onedrive
 Version:    2.4.22
 Release:    1%{?dist}
@@ -59,8 +65,13 @@ make
 %{_docdir}/%{name}
 %{_bindir}/%{name}
 %if 0%{?with_systemd}
+%if 0%{?rhel_7}
+%{_unitdir}/%{name}.service
+%{_unitdir}/%{name}@.service
+%else
 %{_userunitdir}/%{name}.service
 %{_unitdir}/%{name}@.service
+%endif
 %else
 %{_bindir}/onedrive_service.sh
 /etc/init.d/onedrive