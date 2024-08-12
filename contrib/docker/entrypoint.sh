4b6b3791b66cd68444eeb4cc0fdc2fb1477e5718
abraunegg <alex.braunegg@gmail.com>
2024-08-12 17:16:14 +1000

    Update entrypoint.sh
    * Rework #2729 in favor of this change, supports discussion #2768

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -5,22 +5,29 @@ set +H -euo pipefail
 : ${ONEDRIVE_UID:=$(stat /onedrive/data -c '%u')}
 : ${ONEDRIVE_GID:=$(stat /onedrive/data -c '%g')}
 
-# Create new group using target GID
-if ! odgroup="$(getent group "$ONEDRIVE_GID")"; then
-  odgroup='onedrive'
-  groupadd "${odgroup}" -g "$ONEDRIVE_GID"
-else
-  odgroup=${odgroup%%:*}
-fi
+# Skip user and group creation if running as root
+if [ "$(id -u)" -ne 0 ]; then
+    # Create new group using target GID
+    if ! odgroup="$(getent group "$ONEDRIVE_GID")"; then
+      odgroup='onedrive'
+      groupadd "${odgroup}" -g "$ONEDRIVE_GID"
+    else
+      odgroup=${odgroup%%:*}
+    fi
 
-# Create new user using target UID
-if ! oduser="$(getent passwd "$ONEDRIVE_UID")"; then
-  oduser='onedrive'
-  useradd -m "${oduser}" -u "$ONEDRIVE_UID" -g "$ONEDRIVE_GID"
+    # Create new user using target UID
+    if ! oduser="$(getent passwd "$ONEDRIVE_UID")"; then
+      oduser='onedrive'
+      useradd -m "${oduser}" -u "$ONEDRIVE_UID" -g "$ONEDRIVE_GID"
+    else
+      oduser="${oduser%%:*}"
+      usermod -g "${odgroup}" "${oduser}"
+      grep -qv root <( groups "${oduser}" ) || { echo 'ROOT level privileges prohibited!'; exit 1; }
+    fi
 else
-  oduser="${oduser%%:*}"
-  usermod -g "${odgroup}" "${oduser}"
-  grep -qv root <( groups "${oduser}" ) || { echo 'ROOT level privileges prohibited!'; exit 1; }
+    echo "Running as root, skipping user and group creation."
+    oduser='root'
+    odgroup='root'
 fi
 
 # Default parameters
