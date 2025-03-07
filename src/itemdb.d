d45ca81f3e5d56752b9e6ab797c3530cc8914be8
abraunegg <alex.braunegg@gmail.com>
2025-03-07 11:17:12 +1100

    Update itemdb.d
    * As part of this PR, increase DB version to ensure all old records in the DB are purged

--- src/itemdb.d
+++ src/itemdb.d
@@ -225,7 +225,7 @@ Item makeDatabaseItem(JSONValue driveItem) {
 
 final class ItemDatabase {
 	// increment this for every change in the db schema
-	immutable int itemDatabaseVersion = 15;
+	immutable int itemDatabaseVersion = 16;
 
 	Database db;
 	string insertItemStmt;
