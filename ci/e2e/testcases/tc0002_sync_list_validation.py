247646b70f06a32c7d15e90c1ece1b223af0dc67
abraunegg <alex.braunegg@gmail.com>
2026-03-13 13:59:18 +1100

    Update tc0002_sync_list_validation.py
    * Update SL-0023

--- ci/e2e/testcases/tc0002_sync_list_validation.py
+++ ci/e2e/testcases/tc0002_sync_list_validation.py
@@ -1472,6 +1472,7 @@ class TestCase0002SyncListValidation(E2ETestCase):
                 allowed_exact=[
                     f"{FIXTURE_ROOT_NAME}/README.txt",
                     f"{FIXTURE_ROOT_NAME}/loose.bin",
+                    f"{FIXTURE_ROOT_NAME}/zz_e2e_zz_e2e_sync_list.bin",
                 ],
                 allowed_prefixes=[f"{FIXTURE_ROOT_NAME}/Projects"],
                 forbidden_prefixes=[
@@ -1481,6 +1482,7 @@ class TestCase0002SyncListValidation(E2ETestCase):
                 required_processed=[
                     f"{FIXTURE_ROOT_NAME}/README.txt",
                     f"{FIXTURE_ROOT_NAME}/loose.bin",
+                    f"{FIXTURE_ROOT_NAME}/zz_e2e_zz_e2e_sync_list.bin",
                     f"{FIXTURE_ROOT_NAME}/Projects/Code/JOBXYZ/Exports/file.wav",
                     f"{FIXTURE_ROOT_NAME}/Projects/Code/JOBXYZ/Source/main.txt",
                 ],
