6ebea18b8a436527509b90eb884d43f02ad15b88
abraunegg <alex.braunegg@gmail.com>
2026-03-16 18:49:17 +1100

    Update run.py
    Fix import for tc0027

--- ci/e2e/run.py
+++ ci/e2e/run.py
@@ -35,7 +35,7 @@ from testcases.tc0023_bypass_data_preservation_validation import TestCase0023Byp
 from testcases.tc0024_big_delete_safeguard_validation import TestCase0024BigDeleteSafeguardValidation
 from testcases.tc0025_invalid_character_filename_validation import TestCase0025InvalidCharacterFilenameValidation
 from testcases.tc0026_reserved_device_name_validation import TestCase0026ReservedDeviceNameValidation
-from tc0027_whitespace_trailing_dot_validation import TestCase0027WhitespaceTrailingDotValidation
+from testcases.tc0027_whitespace_trailing_dot_validation import TestCase0027WhitespaceTrailingDotValidation
 
 
 def build_test_suite() -> list:
@@ -71,7 +71,7 @@ def build_test_suite() -> list:
         #TestCase0024BigDeleteSafeguardValidation(),
         TestCase0025InvalidCharacterFilenameValidation(),
         TestCase0026ReservedDeviceNameValidation(),
-        #TestCase0027WhitespaceTrailingDotValidation(),
+        TestCase0027WhitespaceTrailingDotValidation(),
     ]
 
 
