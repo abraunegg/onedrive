d84d2f30e0a9e4c3401cc0339ca2a6ecd48bec11
abraunegg <alex.braunegg@gmail.com>
2026-06-06 08:50:21 +1000

    Update PR
    * Update PR

--- ci/e2e/run.py
+++ ci/e2e/run.py
@@ -74,7 +74,7 @@ from testcases.tc0059_websocket_remote_upload_notification import TestCase0059We
 from testcases.tc0060_monitor_mode_local_move_no_delete_reupload import TestCase0060MonitorModeLocalMoveNoDeleteReupload
 from testcases.tc0061_remote_move_into_skip_dir_reconciliation import TestCase0061RemoteMoveIntoSkipDirReconciliation
 from testcases.tc0062_transfer_metrics_validation import TestCase0062TransferMetricsValidation
-from testcases.tc0063_download_post_chmod_race_reporting import TestCase0063DownloadPostChmodRaceReporting
+from testcases.tc0063_download_sibling_after_case_rename_filesystem_race import TestCase0063DownloadSiblingAfterCaseRenameFilesystemRace
 
 def build_test_suite() -> list:
     return [
@@ -140,9 +140,8 @@ def build_test_suite() -> list:
         #TestCase0060MonitorModeLocalMoveNoDeleteReupload(),
         #TestCase0061RemoteMoveIntoSkipDirReconciliation(),
         #TestCase0062TransferMetricsValidation(),
-        TestCase0063DownloadPostChmodRaceReporting(),
+        TestCase0063DownloadSiblingAfterCaseRenameFilesystemRace(),
     ]
-
 
 def _normalise_case_id(value: str) -> str:
     text = value.strip()
